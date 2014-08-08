95f70423faace86366894be6c941691b4d7670d4
jfreehill <jfreehill@deloitte.com>
2014-08-08 15:45:40 -0600

    App mostly styled, fonts added, etc. Needs more categories...

--- Gruntfile.js
+++ Gruntfile.js
@@ -183,7 +183,7 @@ module.exports = function (grunt) {
         generatedImagesDir: '.tmp/images/generated',
         imagesDir: '<%= yeoman.app %>/images',
         javascriptsDir: '<%= yeoman.app %>/scripts',
-        fontsDir: '<%= yeoman.app %>/styles/fonts',
+        fontsDir: '<%= yeoman.app %>/fonts',
         importPath: './bower_components',
         httpImagesPath: '/images',
         httpGeneratedImagesPath: '/images/generated',
